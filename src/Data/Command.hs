cbd16a04eb3751c637351b40d80e76cb7bedc0b6
ivxvm <ivxvm@protonmail.com>
2020-02-16 18:20:25 +0200

    Better commands and scenarios

--- src/Data/Command.hs
+++ src/Data/Command.hs
@@ -2,29 +2,47 @@ module Data.Command
     ( CommandF(..)
     , Command
     , resolve
-    , exec
-    , inspect
+    , cutAudioSegment
+    , cutVideoSegment
+    , cutSubtitlesSegment
+    , combineAudioAndVideo
+    , combineSubtitlesAndVideo
+    , persist
     )
 where
 
 import           Data.Source
 import           Control.Monad.Free            as Free
 
-data CommandF r
-    = Resolve Source (String -> r)
-    | Exec String (Bool -> r)
+data CommandF source resolvedSource media r
+    = Resolve source (resolvedSource -> r)
+    | CutAudioSegment resolvedSource (media -> r)
+    | CutVideoSegment resolvedSource (media -> r)
+    | CutSubtitlesSegment resolvedSource (media -> r)
+    | CombineAudioAndVideo media media (media -> r)
+    | CombineSubtitlesAndVideo media media (media -> r)
+    | Persist media
     deriving (Functor)
 
-type Command = Free CommandF
+type Command source resolvedSource media = Free (CommandF source resolvedSource media)
 
-resolve :: Source -> Command String
+resolve :: source -> Command source resolvedSource media resolvedSource
 resolve source = liftF $ Resolve source id
 
-exec :: String -> Command Bool
-exec code = liftF $ Exec code id
+cutAudioSegment :: resolvedSource -> Command source resolvedSource media media
+cutAudioSegment resolvedSource = liftF $ CutAudioSegment resolvedSource id
 
-inspect :: Command () -> [String]
-inspect (Pure ()) = []
-inspect (Free (Resolve src r)) =
-    ("Resolve (" ++ show src ++ ")") : inspect (r "__resolve_result__")
-inspect (Free (Exec code r)) = ("Exec '" ++ code ++ "'") : inspect (r True)
+cutVideoSegment :: resolvedSource -> Command source resolvedSource media media
+cutVideoSegment resolvedSource = liftF $ CutVideoSegment resolvedSource id
+
+cutSubtitlesSegment :: resolvedSource -> Command source resolvedSource media media
+cutSubtitlesSegment resolvedSource = liftF $ CutSubtitlesSegment resolvedSource id
+
+combineAudioAndVideo :: media -> media -> Command source resolvedSource media media
+combineAudioAndVideo audio video = liftF $ CombineAudioAndVideo audio video id
+
+combineSubtitlesAndVideo :: media -> media -> Command source resolvedSource media media
+combineSubtitlesAndVideo subtitles video = liftF $ CombineSubtitlesAndVideo subtitles video id
+
+persist :: media -> Command source resolvedSource media ()
+persist media = liftF $ Persist media
--- src/Scenario/CutSegment.hs
+++ src/Scenario/CutSegment.hs
@@ -6,8 +6,26 @@ where
 import           Data.Source
 import           Data.Command
 
-cutSegment :: Source -> String -> String -> Command ()
-cutSegment source from to = do
-    input <- resolve source
-    exec $ "ffmpeg -i " ++ input
-    return ()
+data Options = Options
+    { fromTime :: String
+    , toTime :: String
+    , audioOnly :: Bool
+    , includeSubtitles :: Bool
+    }
+
+cutSegment :: Options -> Source -> Command Source resolvedSource media ()
+cutSegment Options {..} source = do
+    resolvedSource <- resolve source
+    if
+        | audioOnly -> do
+            audio <- cutAudioSegment resolvedSource
+            persist audio
+        | otherwise -> do
+            video <- cutVideoSegment resolvedSource
+            if
+                | includeSubtitles -> do
+                    subtitles <- cutSubtitlesSegment resolvedSource
+                    result    <- combineSubtitlesAndVideo subtitles video
+                    persist result
+                | otherwise -> do
+                    persist video
